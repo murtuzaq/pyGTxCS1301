2bee797042a1f8626f125c9a89933857665e1630
Murtuza Quaizar <mquaizar@chefman.com>
2020-09-03 16:48:11 -0400

    frst two printf example

---
+++ CS1301/py1_2_3_ex1_HelloCS1301.py
@@ -0,0 +1,15 @@
+#Print "Hello, CS1301" to the console.
+#
+#Do not print the quotation marks themselves,
+#only the text inside them.
+
+print("Hello, CS1301")
+
+########################################
+# We expected your code to print this:
+#
+# Hello, CS1301
+#
+# It printed this:
+#
+# Hello, CS1301
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pyGTxCS1301
+
+Computing in Python I: Fundamentals and Procedural Programming;
+
+Extra Resources:
+
+[Google Python Code](https://developers.google.com/edu/python/)
+[Harvard CS50] (https://cs50.harvard.edu/x/2020/)
